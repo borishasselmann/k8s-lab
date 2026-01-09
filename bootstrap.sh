29a5323193c0ad741291a260c82ed45ed424a036
Boris Hasselmann <boris.hasselmann@gmail.com>
2026-01-09 23:48:43 +0000

    Add waiting mechanism for ArgoCD deployment in bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 kubectl apply -f infrastructure/argocd/namespace.yaml
 kubectl apply -f infrastructure/argocd/install.yaml
+echo "Waiting for ArgoCD deployment to be created..."
+until kubectl -n argocd get deployment argocd-server &>/dev/null; do
+  sleep 2
+done
 kubectl wait --for=condition=available --timeout=300s deployment/argocd-server -n argocd
 kubectl patch deployment argocd-server -n argocd --type='json' -p='[{"op": "add", "path": "/spec/template/spec/containers/0/args/-", "value": "--insecure"}]'
 kubectl apply -f infrastructure/argocd/ingress.yaml
